0f7b67692d33381c33f979a3ecc2d7282ac2283b
Lofd <random@gmail.net>
2020-07-28 15:25:57 -0400

    moved food collision function over to body

--- Engine/Snake.h
+++ Engine/Snake.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "Location.h"
 #include "Board.h"
-#include "Food.h"
 
 class Snake {
 	Location spot;
 	Color c;
 public:
 	Snake() = default;
-	Snake(int pos, int pos2, const Board& b) : spot{ pos,pos2 }, c{ 255,0,0 } {
+	Snake(int pos, int pos2, const Board& b) : spot{ pos,pos2 }, c{ Colors::Green } {
 	}
-	Snake(int pos, int pos2) : spot{pos, pos2}, c(255,0,0) {}
+	Snake(int pos, int pos2) : spot{ pos, pos2 }, c{ Colors::Green } {}
 	void show(const Board& b) const;
 	Location& getSpot();
 	const Location getSpot1() const;
@@ -21,12 +20,12 @@ public:
 class Body {
 	static constexpr int size = 100;
 	Snake segments[size];
-	int current = 0;
-public :
-	Body() = default;
+	int current;
+public:
+	Body() : current{ 0 } {};
 	void grow();
 	void show1(const Board& b);
-	void change(int x, int y,Snake& snake);
+	void change(int x, int y, Snake& snake);
 	void collided(const Snake& snek) const;
 	bool checkAvailable(int x, int y) const;
 };
